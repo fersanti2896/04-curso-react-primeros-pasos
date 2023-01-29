889f8a2afddc75743c30a84599a82a6c0615ca8e
Fernando Nicolás <fersanti2896@gmail.com>
2023-01-29 16:16:31 -0600

    Variables en HTML y aplicando estilos

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,20 @@
 
+const persona = {
+    name: 'Fernando', 
+    age: 25,
+    profession: 'Programador'
+}
+
+const saludo = () => {
+    return '¡Bienvenido al curso React!';
+}
 
 export const FirstApp = () => {
     return ( 
-        <h1>¡José Fernando N!</h1>
+        <>
+            <h1>{ saludo() }</h1>
+            <p>Soy un subtítulo</p>
+            <code>{ JSON.stringify( persona ) }</code>
+        </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { FirstApp } from './FirstApp';
 
+import './style.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <FirstApp />
