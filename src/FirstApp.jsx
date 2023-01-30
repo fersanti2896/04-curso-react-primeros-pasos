342d4d31c4774035c0c3591bf4824ff30ba98bc4
Fernando Nicolás <fersanti2896@gmail.com>
2023-01-29 23:59:01 -0600

    Props - Comunicación entre componentes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -9,12 +9,15 @@ const saludo = () => {
     return '¡Bienvenido al curso React!';
 }
 
-export const FirstApp = () => {
+export const FirstApp = ({ title, subTitle } ) => {
     return ( 
         <>
             <h1>{ saludo() }</h1>
             <p>Soy un subtítulo</p>
             <code>{ JSON.stringify( persona ) }</code>
+
+            <h2>{ title }</h2>
+            <h3>{ subTitle }</h3>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <FirstApp />
+      <FirstApp title="Hola, Soy Fernando!" subTitle={ 1 }/>
     </React.StrictMode>,
 );
